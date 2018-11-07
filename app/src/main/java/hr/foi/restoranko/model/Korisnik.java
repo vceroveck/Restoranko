c7ccfb37b750ff5212dcc5d8b497c763305143f7
DESKTOP-M6UIFMD\Vinko <vceroveck@foi.hr>
2018-11-07 13:19:08 +0100

    Dodana metoda za registraciju korisnika u klasi Korisnik.

--- app/src/main/java/hr/foi/restoranko/model/Korisnik.java
+++ app/src/main/java/hr/foi/restoranko/model/Korisnik.java
@@ -1,5 +1,12 @@
 package hr.foi.restoranko.model;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Korisnik {
     private String ime;
     private String prezime;
@@ -57,4 +64,16 @@ public class Korisnik {
     public void setLozinka(String lozinka) {
         this.lozinka = lozinka;
     }
+
+
+    public void registrirajKorisnika(){
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("korisnik").child(this.korisnickoIme).setValue(this, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+
+            }
+        });
+    }
+
 }
