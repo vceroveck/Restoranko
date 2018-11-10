b8b9a1b7b90ac9475a9ecf4e718d14142fbeed23
Valentina <valpozar@foi.hr>
2018-11-10 23:24:57 +0100

    Kreirani su klasa Navigation i layout activity_navigation.

---
+++ app/src/main/java/hr/foi/restoranko/Navigation.java
@@ -0,0 +1,13 @@
+package hr.foi.restoranko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
