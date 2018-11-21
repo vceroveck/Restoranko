bda8594049fed2d4c1bd9e1b31f71c8d6d8228cf
Valentina <valpozar@foi.hr>
2018-11-21 02:55:03 +0100

    Dodan LogoutFragment.

--- app/src/main/java/hr/foi/restoranko/Navigation.java
+++ app/src/main/java/hr/foi/restoranko/Navigation.java
@@ -1,6 +1,8 @@
 package hr.foi.restoranko;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,40 +17,37 @@ import android.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Navigation extends AppCompatActivity {
+public class Navigation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth firebaseAuth;
-
-    //private DrawerLayout drawer;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
         firebaseAuth = FirebaseAuth.getInstance();
 
-        //drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer,
-                //R.string.otvoriNavigacija, R.string.zatvoriNavigacija);
-        //drawer.addDrawerListener(toggle);
-        //toggle.syncState();
     }
 
-    //public void onBackPressed(){
-        //if(drawer.isDrawerOpen(GravityCompat.START)){
-            //drawer.closeDrawer(GravityCompat.START);
-        //} else{
-            //super.onBackPressed();
-        //}
-    //}
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
 
-    //private void setSupportActionBar(Toolbar toolbar) {
-    //}
+            case  R.id.nav_odjava:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFragment());
+                Logout();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     private void Logout(){
         firebaseAuth.signOut();
