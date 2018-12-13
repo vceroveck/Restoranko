9ddc3ddb06e2e888a593ba4b9b90299761e8ffbe
Valentina <valpozar@foi.hr>
2018-12-14 00:30:35 +0100

    Kreirana klasa Password i layout activity_password. Dodan izgled ekrana za promjenu lozinke. Implementirana mogućnost promjene korisničke lozinke.

--- app/src/main/java/hr/foi/restoranko/controller/KorisnickiProfil.java
+++ app/src/main/java/hr/foi/restoranko/controller/KorisnickiProfil.java
@@ -1,15 +1,29 @@
 package hr.foi.restoranko.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import hr.foi.restoranko.R;
 
 public class KorisnickiProfil extends AppCompatActivity {
 
+    private Button forgotPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_korisnicki_profil);
+
+        forgotPassword = (Button)findViewById(R.id.btnPromijenitiLozinku);
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(KorisnickiProfil.this, Password.class));
+            }
+        });
     }
 }
